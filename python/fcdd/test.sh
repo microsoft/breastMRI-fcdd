9e62cc6ca95855825c72e91899206ba487dead8e
Felipe <felipe.oviedo@microsoft.com>
2025-05-29 18:34:02 +0000

    - Fix security issues

---
+++ python/fcdd/test.sh
@@ -0,0 +1,6 @@
+python runners/predict_custom.py --model fcdd --task 2
+python runners/predict_custom.py --model fcdd --task 1
+python runners/predict_custom.py --model bce --task 2
+python runners/predict_custom.py --model bce --task 1
+python runners/predict_custom.py --model hsc --task 1
+python runners/predict_custom.py --model hsc --task 2
